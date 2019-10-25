88af083dd9b2a62c5d773df06712b111df7ef30b
Gavin Alberghini <gavinjalberghini@gmail.com>
2019-10-25 17:58:14 -0400

    Initial project, added SteamVR plugin. The Player Prefab will let us use the gloves from the tutorial. I have custom input coming in from the trackpad so walking will not be difficult to role ontop of that.

---
+++ KINE-Unity-Final/Assets/Scripts/Walk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class Walk : MonoBehaviour
+    {
+
+        public SteamVR_Action_Vector2 walkingAction;
+        public Hand hand;
+        public GameObject player;
+
+        private void OnEnable()
+        {
+            if (hand == null)
+                hand = this.GetComponent<Hand>();
+
+            if (walkingAction == null)
+            {
+                Debug.LogError("<b>[SteamVR Interaction]</b> No walking action assigned"); return;
+            }
+
+            walkingAction.AddOnChangeListener(OnWalkingActionChange, hand.handType);
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
+        private void OnWalkingActionChange(SteamVR_Action_Vector2 fromAction, SteamVR_Input_Sources fromSource, Vector2 axis, Vector2 delta)
+        {
+            Debug.Log("AXIS VECTOR: " + axis);
+            Debug.Log("DELTA VECTOR: " + delta);
+        }
+
+    }
+}
